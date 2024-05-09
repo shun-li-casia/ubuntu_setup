7e1603f755034b932dec6ebf3b43e434cfc77b4d
Shun Li <shun.li.at.casia@outlook.com>
2024-05-09 17:21:48 +0800

    update

--- mac_m2_ubuntu_setup/install_ros_and_its_dependences.sh
+++ mac_m2_ubuntu_setup/install_ros_and_its_dependences.sh
@@ -8,42 +8,55 @@
 # @description : install_ros_and_its_dependences
 ######################################################################
 
-#!/bin/bash
-
-find_git_parent() {
+find_setup_root() {
     local current_dir="$1"
-    local parent_dir="$current_dir"
+    local parent_dir
 
-    while [ "$parent_dir" != "/" ]; do
-        if [ -d "$parent_dir/.git" ]; then
-            echo "$parent_dir"
-            return
+    while :; do
+        for target in ".git" ".root" ".vim_root"; do
+            if [ -d "$current_dir/$target" ] || [ -f "$current_dir/$target" ]; then
+                echo "$current_dir"
+                export UBUNTU_SETUP_ROOT="$current_dir"
+                return 0
+            fi
+        done
+        # 如果已经是根目录，停止循环
+        if [[ $current_dir == "/" ]]; then
+            echo "No .git directory or .root file found in the parent directories." >&2
+            return 1
         fi
-        parent_dir=$(dirname "$parent_dir")
+        # 否则，继续向上查找
+        parent_dir=$(dirname "$current_dir")
+        if [[ $parent_dir == "$current_dir" ]]; then break; fi
+        current_dir=$parent_dir
     done
-
-    echo "No .git directory found in parent directories." >&2
-    return 1
 }
 
-ubuntu_setup_root=$(find_git_parent "$PWD")
-if [ $? -eq 0 ]; then
-    export UBUNTU_SETUP_ROOT="$ubuntu_setup_root"
-    echo "Exported GIT_ROOT=$UBUNTU_SETUP_ROOT"
-else
-    echo "Failed to find and export UBUNTU_SETUP_ROOT."
-fi
+# 获取当前脚本的绝对路径
+this_script=$(readlink -f "$0")
+# 获取当前脚本的目录
+script_dir=$(dirname "$this_script")
 
+find_setup_root "$script_dir"
+
+if [[ -n $UBUNTU_SETUP_ROOT ]]; then
+    echo "UBUNTU_SETUP_ROOT set to: $UBUNTU_SETUP_ROOT"
+else
+    echo "Failed to set UBUNTU_SETUP_ROOT."
+fi
 source $UBUNTU_SETUP_ROOT/utility_tool_bash/log_helper.sh
+
+
+# start the install
 
 log_info $(get_cur_line_number)
 execute_with_check "sudo apt update && sudo apt upgrade -y"
 
 log_info $(get_cur_line_number)
-execute_with_check "sudo apt install git vim curl net-tools openssh*"
+execute_with_check "sudo apt install -y git curl net-tools openssh*"
 
 log_info $(get_cur_line_number)
-execute_with_check "sudo apt install libceres-dev libyaml-cpp-dev"
+execute_with_check "sudo apt install -y libceres-dev libyaml-cpp-dev"
 
 log_info $(get_cur_line_number)
 execute_with_check "sudo apt install -y ros-* python3-pip"
