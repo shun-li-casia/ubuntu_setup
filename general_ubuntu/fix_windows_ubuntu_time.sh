31d6a2b2941ea2b2189dd144f48552c840050938
shun li <is.shun.li@outlook.com>
2024-06-26 22:04:58 +0800

    update

---
+++ general_ubuntu/fix_windows_ubuntu_time.sh
@@ -0,0 +1,3 @@
+sudo apt-get install ntpdate -y
+sudo ntpdate time.windows.com
+sudo hwclock --localtime --systohc
---
+++ general_ubuntu/install_sogou_2004.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install fcitx
+sudo apt purge ibus
+sudo apt install libqt5qml5 libqt5quick5 libqt5quickwidgets5 qml-module-qtquick2
+sudo apt install libgsettings-qt1
